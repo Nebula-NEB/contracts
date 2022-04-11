50ded504b95624864408c29ed7ce15b8b82f18f7
Endogen <Endogen@users.noreply.github.com>
2022-04-11 21:53:56 +0200

    Added checks for decimal

--- con_neb_vault_int.py
+++ con_neb_vault_int.py
@@ -5,11 +5,12 @@
 # | |\  |  __/ |_) | |_| | | (_| |  _| |_| | | | ||  __/ |  | | | | (_| | |    \  / (_| | |_| | | |_ 
 # |_| \_|\___|_.__/ \__,_|_|\__,_| |_____|_| |_|\__\___|_|  |_| |_|\__,_|_|     \/ \__,_|\__,_|_|\__|
 #
+# Version 1.2
 
 I = importlib
 
-staking = Hash(default_value=0)
-payouts = Hash(default_value=0)
+staking = Hash(default_value=0.0)
+payouts = Hash(default_value=0.0)
 
 emission_con = Variable()
 total_emission = Variable()
@@ -38,7 +39,13 @@ OPERATORS = [
 @export
 def fund_vault(emission_contract: str, total_emission_amount: float, total_stake_amount: float,
                minutes_till_start: int, start_period_in_minutes: int, minutes_till_end: int):
-    
+
+    assert isinstance(total_emission_amount, decimal), 'Type of total_emission_amount must be float'
+    assert isinstance(total_stake_amount, decimal), 'Type of total_stake_amount must be float'
+    assert isinstance(minutes_till_start, int), 'Type of minutes_till_start must be int'
+    assert isinstance(start_period_in_minutes, int), 'Type of start_period_in_minutes must be int'
+    assert isinstance(minutes_till_end, int), 'Type of minutes_till_end must be int'
+
     assert funded.get() != True, 'Vault is already funded!'
     assert total_emission_amount > 0, 'total_emission_amount not valid!'
     assert total_stake_amount > 0, 'total_stake_amount not valid!'
@@ -46,9 +53,10 @@ def fund_vault(emission_contract: str, total_emission_amount: float, total_stake
     assert start_period_in_minutes >= MIN_STAKE_PERIOD, 'Staking needs to be open for at least 2 days!'
     assert minutes_till_end > 0 and minutes_till_end <= MAX_RUNTIME, 'minutes_till_end not valid!'
 
+    ec = I.import_module(emission_contract)
     emission_con.set(emission_contract)
 
-    current_stake.set(0)
+    current_stake.set(decimal(0))
     total_emission.set(total_emission_amount)
     total_stake.set(total_stake_amount)
 
@@ -56,7 +64,7 @@ def fund_vault(emission_contract: str, total_emission_amount: float, total_stake
     start_date_end.set(start_date.get() + datetime.timedelta(minutes=start_period_in_minutes))
     end_date.set(start_date_end.get() + datetime.timedelta(minutes=minutes_till_end))
 
-    single_fee = (total_emission.get() / 100 * NEB_FEE) / len(OPERATORS)
+    single_fee = (total_emission.get() / 100 * decimal(NEB_FEE)) / decimal(len(OPERATORS))
 
     for address in OPERATORS:
         I.import_module(emission_con.get()).transfer_from(
@@ -81,6 +89,7 @@ def stake(neb_amount: float):
     assert_active()
 
     assert neb_amount > 0, 'Negative amounts are not allowed'
+    assert isinstance(neb_amount, decimal), 'Type of neb_amount must be float'
     assert now > start_date.get(), f'Staking not started yet: {start_date.get()}'
     assert now < start_date_end.get(), f'Staking period ended: {start_date_end.get()}'
 
@@ -99,7 +108,7 @@ def stake(neb_amount: float):
 def unstake():
     assert_active()
 
-    assert staking[ctx.caller] != 0, f'Address is not staking!'
+    assert staking[ctx.caller] > 0, f'Address is not staking!'
     assert now > end_date.get(), f'End date not reached: {end_date.get()}'
 
     stake_percent = staking[ctx.caller] / current_stake.get() * 100
@@ -115,7 +124,7 @@ def unstake():
 
     I.import_module(LP_VAULT).unlock()
 
-    staking[ctx.caller] = 0
+    staking[ctx.caller] = decimal(0)
     payouts[ctx.caller] = user_emission
 
     return f'Emission: {user_emission} {emission_con.get()}'
